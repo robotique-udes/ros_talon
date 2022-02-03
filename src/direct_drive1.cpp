32a02081270f4c447d6975fd3c93a7232afb69c9
Loic Boileau <boil3205@usherbrooke.ca>
2022-02-03 10:47:19 -0500

    ;

--- src/direct_drive1.cpp
+++ src/direct_drive1.cpp
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
 	//talon.setup(1, modePercentOutput, motor_nb_c);
 	talon.setup(1, modeServoPosition, motor_nb_c);
 
-	talon.setZero()
+	talon.setZero();
 
 	ROS_WARN_STREAM("Motor ID 1 : " << motor_nb_c);
 	ros::spin();
